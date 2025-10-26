53669ceb6515f05e85dffa659d36d99221c496fc
Sara.G <151964803+SaraGaire@users.noreply.github.com>
2025-10-26 20:04:49 +0100

    Create error.js

---
+++ frontend/pages/error.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-2">404 - Page Not Found</h1>
+      <p className="text-gray-600">Looks like this page doesn’t exist.</p>
+    </main>
+  )
+}
